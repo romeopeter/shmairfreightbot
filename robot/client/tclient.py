51ba853b0148a719af9e45ef2cc78657dbe24577
Romeo-Peter <romeomeshack34@gmail.com>
2021-01-21 02:39:43 +0100

    Added dispatcher methods to handle callback functions and to start client

--- robot/client/tclient.py
+++ robot/client/tclient.py
@@ -42,12 +42,19 @@ class Client(CommandHandlerCallbacks):
 
         return self.bot.get_me()
 
-    def set_handler(self) -> None:
+    def set_dispatchers(self) -> None:
         """
-        Dispatches callback function in to process user inputs via 'CommandHandler' method
+        Calls dispatcher to register handler via 'CommandHandler' method.
         """
 
         self.dispatcher.add_handler((CommandHandler("start", self.start_callback)))
+        self.dispatcher.add_handler((CommandHandler("setname", self.set_name_callback)))
+        self.dispatcher.add_handler(
+            (CommandHandler("setemail", self.set_email_callback))
+        )
+        self.dispatcher.add_handler(
+            (CommandHandler("setphonenumber", self.set_phone_callback))
+        )
 
     def start_client(self) -> None:
         """
@@ -58,10 +65,21 @@ class Client(CommandHandlerCallbacks):
         None -- Returns nothing.
         """
 
-        self.set_handler()
+        self.set_dispatchers()
         self.updater.start_polling()
+
+    def stop_client(self):
+        # Stop client from running when the Ctrl + c signal is detected
+        self.updater.idle()
 
 
 if __name__ == "__main__":
     tclient = Client(token=config("TELEGRAM_API_TOKEN", cast=str))
+
+    print("Starting Bot...")
+
+    # Start client
     tclient.start_client()
+
+    # Start client
+    tclient.stop_client()
