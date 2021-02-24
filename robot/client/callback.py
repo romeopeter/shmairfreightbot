b5eb4ca4c100a398a218aff2d919f81e1f967205
Romeo-Peter <romeomeshack34@gmail.com>
2021-02-24 03:29:56 +0100

    Added postgresql statement to insert data into database and method to parse double quotes to single quotes when passed as value in postgres

--- robot/client/callback.py
+++ robot/client/callback.py
@@ -17,18 +17,13 @@ from robot.storage.dbconnection import Connection
 class CommandHandlerCallbacks:
     """Mix-in classes defines Telegram client callback methods"""
 
-    @staticmethod
-    def shipment_details(key: str = "status", value: Any = None):
-        """Initiate register details. This is a class attribute rather than an attribute of the instance of the class"""
-        details: dict = {}
-        details[key] = value
-
-        return details
+    def __init__(self) -> None:
+        self._store = self.store_details()
 
     def start_callback(self, update, context) -> None:
         """Processes '/start' command from user"""
 
-        customer_name = update.message.from_user.first_name
+        customer_name = str(update.message.from_user.first_name)
         reply_text = update.message.reply_text
 
         # inline keyboard command
@@ -46,16 +41,18 @@ class CommandHandlerCallbacks:
             reply_text(f"Hello {update.message.from_user.first_name} 👋")
 
             # Store name
-            self.shipment_details(key="status", value="updated")
-            self.shipment_details(key="custormer_name", value=customer_name)
-
-            # Reply user with inline keyboard to selection options
-            reply_text(
-                "Do you want to register shipment or track shipment?\nUse the buttons below to answer👇",
-                reply_markup=InlineKeyboardMarkup(keyboard),
+            store_customer_name = self._store.create(
+                f"INSERT INTO shipment (name) VALUES ({self._remove_double_quotes(customer_name)});"
             )
 
-        return
+            if store_customer_name:
+                # Reply user with inline keyboard to selection options
+                reply_text(
+                    "Do you want to register shipment or track shipment?\nUse the buttons below to answer👇",
+                    reply_markup=InlineKeyboardMarkup(keyboard),
+                )
+
+            return
 
     def inline_button_callback(self, update: Update, context: CallbackContext) -> None:
         """Get button that was tapped on. This is base on what is assigned to 'callback-data' param in the 'InlineKeyboardButton' object"""
@@ -92,7 +89,7 @@ class CommandHandlerCallbacks:
         reply_text = update.message.reply_text
 
         try:
-            user_email: str = str(context.args[0]).strip()
+            user_email = str(context.args[0]).strip()
 
             # Validate user email
             email_regex = (
@@ -100,17 +97,20 @@ class CommandHandlerCallbacks:
                 or "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$"
             )
 
+            # Store email
             if self._validation_regex(email_regex, user_email):
-                self.shipment_details(key="email", value=user_email)
-                reply_text("Success! Email set. /help")
+                store_email = self._store.create(
+                    f"INSERT INTO shipment (email) VALUES ({self._remove_double_quotes(user_email)});"
+                )
+
+                if store_email:
+                    reply_text("Success! Email set. /help")
             else:
                 reply_text(
                     "Invalid email addresss\!\n hint: /setemail _info@domain\.com_",
                     parse_mode="MarkdownV2",
                 )
-        except (IndexError, ValueError) as e:
-            print(e)
-
+        except (IndexError, ValueError):
             reply_text(
                 "Email addresss not set\!\n hint: /setemail _Your email\. e\.g\: info@domain\.com_",
                 parse_mode="MarkdownV2",
@@ -122,24 +122,25 @@ class CommandHandlerCallbacks:
         reply_text = update.message.reply_text
 
         try:
-            user_phone_number: str = str(context.args[0]).strip()
+            phone_number = str(context.args[0]).strip()
 
             # Validate phone number
             phone_regex = "([0-9]{11})"
 
-            if self._validation_regex(phone_regex, user_phone_number):
-                self.shipment_details(
-                    key="customer_phone_number", value=user_phone_number
+            # Store phone number
+            if self._validation_regex(phone_regex, phone_number):
+                store_pnumber = self._store.create(
+                    f"INSERT INTO shipment (phone_number) VALUES ({self._remove_double_quotes(phone_number)});"
                 )
-                reply_text("Success! Phone number set. /help")
+
+                if store_pnumber:
+                    reply_text("Success! Phone number set. /help")
             else:
                 reply_text(
                     "Invalid phone number\!\n hint: /setphonenumber _Your phone number_",
                     parse_mode="MarkdownV2",
                 )
-        except (IndexError, ValueError) as e:
-            print(e)
-
+        except (IndexError, ValueError):
             reply_text(
                 "Phone number not set\!\n hint: /setphonenumber _Your phone number_",
                 parse_mode="MarkdownV2",
@@ -150,35 +151,21 @@ class CommandHandlerCallbacks:
         reply_text = update.message.reply_text
 
         try:
-            item_name: str = str(context.args[0]).strip()
-            self.shipment_details(key="item_name", value=item_name)
+            item_name = str(context.args[0]).strip()
 
-            reply_text("Success! Item name set. /help")
-        except (IndexError, ValueError) as e:
-            print(e)
+            # Store item name
+            store_item_name = self._store.create(
+                f"INSERT INTO shipment (item_name) VALUES ({self._remove_double_quotes(item_name)});"
+            )
 
+            if store_item_name:
+                reply_text("Success! Item name set. /help")
+
+        except (IndexError, ValueError):
             reply_text(
                 "Item name not set\!\n hint: /setitemname _package name_",
                 parse_mode="MarkdownV2",
             )
-
-    def set_tracking_number_callback(
-        self, update: Update, context: CallbackContext
-    ) -> None:
-        """Process '/settrackingnumber' command from user"""
-        reply_text = update.message.reply_text
-
-        try:
-            tracking_id = str(context.args[0]).strip()
-            self.shipment_details(key="tracking_id", value=tracking_id)
-
-            reply_text("Success! Tracking number set. /help")
-        except (IndexError, ValueError) as e:
-            print(e)
-
-            error_response = """Tracking id not set or Invalid id\!\n hint: /settrackingnumber _shipment tracking number_"""
-
-            reply_text(error_response, parse_mode="MarkdownV2")
 
     def set_carrier_callback(self, update: Update, context: CallbackContext) -> None:
         """Process '/setcarrier' command from user"""
@@ -186,18 +173,45 @@ class CommandHandlerCallbacks:
 
         try:
             carrier_name = str(context.args[0]).strip()
-            self.shipment_details(key="carrier_name", value=carrier_name)
 
-            reply_text("Success! Carrier name set. /help")
-        except (ValueError, IndexError) as e:
-            print(e)
+            # Store carrier
+            store_carrier = self._store.create(
+                f"INSERT INTO shipment (carrier_name) VALUES ({self._remove_double_quotes(carrier_name)});"
+            )
 
+            if store_carrier:
+                reply_text("Success! Carrier name set. /help")
+        except (ValueError, IndexError):
             error_response = """Carrier name not set\.\n hint: /setcarriername _shipment tracking number_"""
+
+            reply_text(error_response, parse_mode="MarkdownV2")
+
+    def set_tracking_number_callback(
+        self, update: Update, context: CallbackContext
+    ) -> None:
+        """Process '/settrackingnumber' command from user"""
+
+        reply_text = update.message.reply_text
+
+        try:
+            tracking_id = str(context.args[0]).strip()
+
+            # Store tracking id
+            store_tracking_id = self._store.create(
+                f"INSERT INTO shipment (tracking_id) VALUES ({self._remove_double_quotes(tracking_id)});"
+            )
+
+            if store_tracking_id:
+                reply_text("Success! Tracking number set. /help")
+        except (IndexError, ValueError):
+
+            error_response = """Tracking id not set or Invalid id\!\n hint: /settrackingnumber _shipment tracking number_"""
 
             reply_text(error_response, parse_mode="MarkdownV2")
 
     def set_address_callback(self, update: Update, context: CallbackContext) -> None:
         """Process '/setaddress' command"""
+
         reply_text = update.message.reply_text
 
         try:
@@ -207,19 +221,22 @@ class CommandHandlerCallbacks:
             state = str(context.args[3]).strip() or ""
             zip_code = str(context.args[4]).strip() or ""
 
-            if company or street or city or state or zip_code:
+            # Store address
+            company_string,
+            stree_stringt,
+            city_string,
+            state_string,
+            zip_code_string = (
+            self._remove_double_quotes(company, street, city, state, zip_code)
+            )
 
-                address = {
-                    "company": company,
-                    "street": street,
-                    "city": city,
-                    "state": state,
-                    "zip_code": zip_code,
-                }
+            if company_string or stree_stringt or city_string or state_string or zip_code_string:
+                store_address = self._store.create(
+                    f"INSERT INTO address (street,city,state,zip_code,company) VALUES ({street_string},{city_string},{state_string},{zip_code_string},{company_string});"
+                )
 
-                self.shipment_details(key="addresss", value=address)
-
-                reply_text("Success! Address set. /help")
+                if store_address:
+                    reply_text("Success! Address set. /help")
 
                 return
         except (ValueError, IndexError) as e:
@@ -244,10 +261,6 @@ class CommandHandlerCallbacks:
         else:
             return False
 
-    def return_shipment_details(self) -> dict:
-        """ Checks if shipment details available, then return it."""
-        return self.shipment_details()
-
     def store_details(self) -> Connection:
         """
         Initializes and returns 'Connection' object to connect to database
@@ -265,3 +278,40 @@ class CommandHandlerCallbacks:
         )
 
         return connect_db
+
+    def _remove_double_quotes(self, *args):
+        """
+        Parses strings with double quotes to single quote. Returns generator object if multple elements are passed in. Object must converted to 'List'
+
+        Parameters
+        ----------
+        *args -- allows multple non keyword arguments.
+
+        Returns
+        ------
+        Two data type. Can't say for now.
+
+        Usage
+        ----
+        >>> self.name = "John Doe"
+        >>>
+        >>> name_newstring = self._remove_double_quotes(self.name)
+        >>> name_newstring
+        >>> # 'John Doe'
+        >>>
+        >>> self.company = "XYZ Ltd"
+        >>> self.state = "FCT"
+        >>> self.city = "FCT"
+        >>> self.zip_code = "0000"
+        >>>
+        >>> address_newstring = self._remove_double_quotes(self.company,self.state,self.city,self.zip_code)
+        >>> List(address_newstring)
+        >>> # ['XYZ Ltd','FCT','FCT','0000']
+        """
+
+        if len(args) == 1:
+            return args[0].replace('"','')
+
+        for n in args:
+            yield n.replace('"','')
+
