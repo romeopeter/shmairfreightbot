289ed0782bf58a5f058bbe39f36fa9529d755057
Romeo-Peter <romeomeshack34@gmail.com>
2021-04-06 00:47:07 +0100

    Refactored code

--- robot/tracker/webhook_server.py
+++ robot/tracker/webhook_server.py
@@ -1,17 +1,16 @@
 from flask import Flask, request
+
 import easypost
 from decouple import config
 
 app = Flask(__name__)
 
 
-@app.route("/easypost-webhook", methods=["GET", "POST"])
+@app.route("/easypost-webhook", methods=["GET"])
 def process_webhook():
+    """Recieve and process tracker webhook alerts"""
 
-    """Check if wehbook is about tracker event
-    If key of 'object' has 'Event' and key 'description' has 'tracker.updated' then it is a tracker event"""
-
-    # Check data returned before proceeding
+    # Check if webhook is a tracker event before proceeding.
     webhook_request = request.get_json()
 
     if (
@@ -26,14 +25,16 @@ def process_webhook():
         # Check delivery status on package and update customer accordingly
         message = ""
         if tracker.status == "delivered":
-            message = +"Your package has been delivered\n"
+            message += "Your package has been delivered\n"
         else:
             for tracking_details in tracker.tracking_details:
                 if tracking_details.status == tracker.status:
-                    message = +(
-                        f"{tracker.carrier} says: {tracking_details.message} in {tracking_details.tracking_location.city}, {tracking_details.tracking_location.country}"
-                    )
+                    message += f"{tracker.carrier} says: {tracking_details.message} in {tracking_details.tracking_location.city}, {tracking_details.tracking_location.country}"
                     break
         return "Update on package sent to customer via SMS"
     else:
         return "Request is not a Tracker event. Waiting event..."
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=1234)
