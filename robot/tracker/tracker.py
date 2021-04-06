c0f48ead89320b597c110b175fffa9e408eb5345
Romeo-Peter <romeomeshack34@gmail.com>
2021-04-06 01:24:31 +0100

    Added webhooks methods

--- robot/tracker/tracker.py
+++ robot/tracker/tracker.py
@@ -1,8 +1,6 @@
 import easypost
 from decouple import config
 
-from . import webhook_server
-
 
 class Tracker:
     """Tracker object uses EasyPost service for tracking shipment"""
@@ -10,6 +8,8 @@ class Tracker:
     def __init__(self, client_id) -> None:
         """Initializes 'Tracker' object and sets API key"""
         easypost.api_key = client_id
+
+    # ------Tracker API------
 
     def create_tracker(self, tracking_code: str, carrier: str = None) -> dict:
         """Create a tracker for package when `tracking_code` and `carrier` parameter values are passed in"""
@@ -37,14 +37,31 @@ class Tracker:
 
         return track_all
 
+    # -------Webhooks-------
+
+    def create_webhook(self, url: str) -> dict:
+        """Create a webhook. provide URL paramater to send notifications to"""
+        return easypost.Webhook.create(ur=url)
+
+    def retrieve_webhook(self, id: str) -> dict:
+        """Retrieve a webhook by id"""
+        return easypost.webhook.retrieve(id=id)
+
+    def list_webhooks(self) -> dict:
+        """Retrieve list of webhooks available to authenticated user"""
+        pass
+
 
 if __name__ == "__main__":
     try:
         tracker = Tracker(client_id=config("EASY_POST_API_TOKEN", cast=str))
-        # retrieve_tracker = tracker.retrieve_tracker(
-        #     id="trk_04b2ae3786b44329a2b62a0eea9279b8"
-        # )
+        print("Crendential sent to tracker")
+
+        print("Creating webhook url")
+        tracker.create_webhook(url="https://b4f323e4c0b6.ngrok.io")
+        print("Webhook created")
+
+        tracker.create_tracker(tracking_code="EZ2000000002", carrier="UPS")
+        print("Tracker created")
     except Exception as e:
         print(e)
-
-# NOTE: Run server from within 'tracker.py'. Idealy, tracker is created before running server.
